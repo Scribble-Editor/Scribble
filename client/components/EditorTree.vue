adb4914ca10bcb63abf0c62b4dc62f0db32af15a
Logan Crandall <crandall.logan@gmail.com>
2019-10-18 21:59:03 -0400

    tree item sort now does not follow letter case

--- client/components/EditorTree.vue
+++ client/components/EditorTree.vue
@@ -10,27 +10,51 @@ import EditorTreeItem from '~/components/EditorTreeItem'
 export default {
   name: 'EditorTree',
   components: { EditorTreeItem },
-  computed: {
-    items () {
+  data () {
+    return {
+      items: []
+    }
+  },
+  mounted () {
+    this.updateItems()
+    this.$root.$on('updateTree', this.updateItems)
+  },
+  methods: {
+    updateItems () {
+      // Takes an array containing the file listing structure
+      // ie { label: "", ?children: [] }
       const insertDocument = (structures, path) => {
         const isDirectory = path.includes('/')
+
+        // If we are inserting a directory
         if (isDirectory) {
           const directoryName = path.substr(0, path.indexOf('/', 1)).replace(/\//g, '')
           const directoryInStructure = structures.find(item => item.label === directoryName)
+
+          // If the current directory is not in the structure yet
           if (!directoryInStructure) {
+            // create a new directory structure and push it to the array
             structures.push({ label: directoryName, children: [] })
             structures.sort((a, b) => {
               if ((a.children && b.children) || (!a.children && !b.children)) {
-                return a.label > b.label
+                return a.label.toLowerCase() > b.label.toLowerCase()
               }
               return !a.children
             })
           }
+          // Insert the next directory/document recursively
           insertDocument(structures.find(item => item.label === directoryName).children, path.substr(path.indexOf('/', 1) + 1))
+
+        // If we are inserting a file
         } else {
           const fileName = path
           structures.push({ label: fileName })
-          structures.sort((a, b) => a.label > b.label)
+          structures.sort((a, b) => {
+            if ((a.children && b.children) || (!a.children && !b.children)) {
+              return a.label.toLowerCase() > b.label.toLowerCase()
+            }
+            return !a.children
+          })
         }
       }
 
@@ -40,7 +64,7 @@ export default {
         insertDocument(items, document)
       }
 
-      return items
+      this.items = items
     }
   }
 }
