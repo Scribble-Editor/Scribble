72b833cb233ec3b4ac11de363da249e52f6ecd89
Logan Crandall <crandall.logan@gmail.com>
2019-10-20 23:36:07 -0400

    Fix File > New File

--- client/components/EditorMenuFileNewfile.vue
+++ client/components/EditorMenuFileNewfile.vue
@@ -14,14 +14,24 @@ export default {
         inputAttrs: { placeholder: 'e.g. myFile.cpp' },
         trapFocus: true,
         onConfirm: (fileName) => {
-          this.createFile(fileName)
-          this.changeSelectedFile(fileName)
+          // TODO: Automatically detect mode during file creation
+
+          if (this.$store.state.documents[fileName]) {
+            this.$buefy.toast.open({
+              duration: 5000,
+              message: 'A file with that name already exists',
+              type: 'is-danger'
+            })
+          } else {
+            this.addDocument({ name: fileName, content: '', mode: 'text' })
+            this.$root.$emit('openDocument', fileName)
+            this.$root.$emit('updateTree')
+          }
         }
       })
     },
     ...mapMutations({
-      createFile: 'openFiles/createFile',
-      changeSelectedFile: 'openFiles/changeSelectedFile'
+      addDocument: 'documents/add'
     })
   }
 }
