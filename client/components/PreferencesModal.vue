9138cb5fa713671b3e4ba7ccb13e2317cbb8cdc5
link00000000 <crandall.logan@gmail.com>
2019-10-27 19:00:35 -0400

    Add saving message to preferences modal

--- client/components/PreferencesModal.vue
+++ client/components/PreferencesModal.vue
@@ -6,7 +6,11 @@
           Preferences
         </p>
       </header>
-      <section class="modal-card-body" />
+      <section class="modal-card-body">
+        <b-message type="is-info">
+          Settings are automatically saved when changed.
+        </b-message>
+      </section>
       <footer class="modal-card-foot">
         <button class="button is-primary" type="button" @click="$parent.close()">
           Done
