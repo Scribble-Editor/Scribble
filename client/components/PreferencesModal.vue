6433ef2c604a660a7b74451834a6c62eb498d95c
link00000000 <crandall.logan@gmail.com>
2019-10-27 19:31:03 -0400

    Add editor theme option

--- client/components/PreferencesModal.vue
+++ client/components/PreferencesModal.vue
@@ -7,6 +7,22 @@
         </p>
       </header>
       <section class="modal-card-body">
+        <!-- Theme Selection -->
+        <b-field label="Editor Theme" label-position="on-border">
+          <b-select
+            icon="palette"
+            :value="selectedTheme"
+            @input="changeTheme"
+          >
+            <option
+              v-for="theme in themes"
+              :key="theme"
+              :value="theme"
+            >
+              {{ theme }}
+            </option>
+          </b-select>
+        </b-field>
         <b-message type="is-info">
           Settings are automatically saved when changed.
         </b-message>
@@ -21,7 +37,27 @@
 </template>
 
 <script>
+import { mapMutations } from 'vuex'
+import { themesByName, themes } from '~/plugins/ace-themelist'
+
 export default {
-  name: 'PreferencesModal'
+  name: 'PreferencesModal',
+  computed: {
+    selectedTheme () {
+      return themesByName[this.$store.state.userConfig.theme].caption
+    },
+    themes () {
+      return Object.keys(themesByName).map(key => themesByName[key].caption).sort()
+    }
+  },
+  methods: {
+    changeTheme (newTheme) {
+      const theme = themes.find(theme => theme && theme.caption === newTheme)
+      this.updateUserConfig({ theme: theme.name })
+    },
+    ...mapMutations({
+      updateUserConfig: 'userConfig/update'
+    })
+  }
 }
 </script>
