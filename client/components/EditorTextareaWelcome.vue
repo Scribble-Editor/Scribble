6e8dd9e0fbc6780d4f35255212306671e105d551
link00000000 <crandall.logan@gmail.com>
2019-11-07 00:00:29 -0500

    Add new file button to welcome screen

--- client/components/EditorTextareaWelcome.vue
+++ client/components/EditorTextareaWelcome.vue
@@ -10,7 +10,7 @@
       <div class="image">
         <img src="~/assets/EditorTextareaWelcomeIllustration.svg" alt="Welcome Illustration">
       </div>
-      <button class="button is-primary">
+      <button class="button is-primary" @click="newFile">
         New File
       </button>
     </div>
@@ -18,8 +18,36 @@
 </template>
 
 <script>
+import { mapMutations } from 'vuex'
+import { getModeForPath } from '~/plugins/ace-modelist'
+
 export default {
-  name: 'EditorTextareaWelcome'
+  name: 'EditorTextareaWelcome',
+  methods: {
+    newFile () {
+      this.$buefy.dialog.prompt({
+        message: 'File name:',
+        inputAttrs: { placeholder: 'e.g. myFile.cpp' },
+        trapFocus: true,
+        onConfirm: (fileName) => {
+          if (this.$store.state.documents[fileName]) {
+            this.$buefy.toast.open({
+              duration: 5000,
+              message: 'A file with that name already exists',
+              type: 'is-danger'
+            })
+          } else {
+            this.addDocument({ name: fileName, content: '', mode: getModeForPath(fileName).name })
+            this.$root.$emit('openDocument', fileName)
+            this.$root.$emit('updateTree')
+          }
+        }
+      })
+    },
+    ...mapMutations({
+      addDocument: 'documents/add'
+    })
+  }
 }
 </script>
 
