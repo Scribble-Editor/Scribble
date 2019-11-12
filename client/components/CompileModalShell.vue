05c05ccbe88b608001243847f77603abbbf56186
link00000000 <crandall.logan@gmail.com>
2019-11-11 22:47:49 -0500

    Fix malformed error output

--- client/components/CompileModalShell.vue
+++ client/components/CompileModalShell.vue
@@ -83,7 +83,7 @@ export default {
       if (wasClean) {
         this.print('Connection closed')
       } else {
-        this.print('Connection closed unexpectedly. Error Code: ' + code + ', ' + reason)
+        this.print('Connection closed unexpectedly. Error Code: ' + code + (reason ? ', ' + reason : ''))
       }
       this.connectionOpen = false
     },
