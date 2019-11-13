04779ec8efdcbb00056a0073e433a1331c9cbc62
link00000000 <crandall.logan@gmail.com>
2019-11-12 23:22:00 -0500

    Remove boilerplate styling

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,66 +1,13 @@
 <template>
-  <div class="container">
-    <div>
-      <h1 class="title">
-        scribble
-      </h1>
-      <h2 class="subtitle">
-        ISP Term Project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentationisisisisis
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
-  </div>
+  <span>GET /</span>
 </template>
 
 <script>
 export default {
+  layout: 'default'
 }
 </script>
 
 <style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
 
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
 </style>
