851da986da53dd208e57be35b086a57b510e714d
Logan Crandall <crandall.logan@gmail.com>
2019-11-17 00:02:33 -0500

    Add sign up page (#70)

---
+++ client/layouts/form.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <Navbar />
+    <div class="form-wrapper">
+      <nuxt />
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.form-wrapper {
+  max-width: 600px;
+  margin: 0 auto;
+  padding: 0 2rem;
+}
+</style>
+
+<script>
+import Navbar from '~/components/Navbar'
+
+export default {
+  components: { Navbar }
+}
+</script>
