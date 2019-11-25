de9a28ffd68d42a8ee6ffb79fa0fb4d98d03be92
link00000000 <crandall.logan@gmail.com>
2019-11-25 10:02:23 -0500

    i do not understand why git dropped these files on the way to github

---
+++ client/components/EditorMenuFileHelp.vue
@@ -0,0 +1,19 @@
+<template>
+  <span @click="gotoDocumentation">Help</span>
+</template>
+
+<script>
+export default {
+  name: 'EditorMenuFileHelp',
+  computed: {
+    documentationLink () {
+      return process.env.documentationURI
+    }
+  },
+  methods: {
+    gotoDocumentation () {
+      window.location = this.documentationLink
+    }
+  }
+}
+</script>
